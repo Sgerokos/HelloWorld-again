6ab62c0bc138734905b216c6f125d313a63570ef
Sgerokos <sgerokos@nyit.edu>
2018-10-03 00:07:13 -0400

    Add files via upload

---
+++ Helloworld.java
@@ -0,0 +1,23 @@
+
+public class Helloworld {
+	
+	public static void main (String args[]) {
+		
+		System.out.println("Hello World!");
+		<!DOCTYPE html>
+		<html>
+		  <head>
+		    <title>JSDT_static</title>
+		    <script type="text/javascript">
+		      alert("hello world!");
+		    </script>
+		   </head>
+		 <body>
+		   <h1>JSDT Static HTML5 Page</h1>
+		 
+		 </body>
+		</html>
+		
+	}
+
+}
---
+++ hello.java
@@ -0,0 +1,10 @@
+package helloWorld2;
+
+public class hello {
+ public static void main(String []args) 
+ {
+	 System.out.println("This is hello world 2");
+	 
+ }
+}
+
